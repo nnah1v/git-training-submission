d753cdf39dabdd4edc8e0dbecac850bea5ca87e2
Natsuka <nnah1v@gmail.com>
2026-01-28 12:12:49 +0900

    2026/1/28

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -8,24 +8,31 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
-
 /**
  * ユーザー情報 Service
  */
 @Service
 public class UserService {
 
-  /**
-   * ユーザー情報 Repository
-   */
-  @Autowired
-  private UserRepository userRepository;
+	/**
+	 * ユーザー情報 Repository
+	 */
+	@Autowired
+	private UserRepository userRepository;
 
-  /**
-   * ユーザー情報 全検索
-   * @return 検索結果
-   */
-  public List<User> searchAll() {
-    return userRepository.findAll();
-  }
+	/**
+	 * ユーザー情報 全検索
+	 * @return 検索結果
+	 */
+	public List<User> searchAll() {
+		return userRepository.findAll();
+	}
+
+	/**
+	 * ユーザー情報 主キー検索
+	 * @return 検索結果
+	 */
+	public User findById(Long id) {
+		return userRepository.findById(id).get();
+	}
 }
