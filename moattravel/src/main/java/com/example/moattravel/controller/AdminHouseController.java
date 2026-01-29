195738839ca91c03d5061214eb8a1d8e4ada86bf
Natsuka <nnah1v@gmail.com>
2026-01-29 19:00:32 +0900

    2026/1/29 157/1031

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -1,9 +1,9 @@
 package com.example.moattravel.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +23,13 @@ public class AdminHouseController {
 	}
 
 	@GetMapping
-	public String index(Model model, Pageable pageable) {
-		List<House> houses = houseRepository.findAll();
+	public String index(
+			Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+
 		Page<House> housePage = houseRepository.findAll(pageable);
-		model.addAttribute("houses", houses);
 		model.addAttribute("housePage", housePage);
+		model.addAttribute("houses", housePage.getContent());
 		return "admin/houses/index";
 	}
 }
