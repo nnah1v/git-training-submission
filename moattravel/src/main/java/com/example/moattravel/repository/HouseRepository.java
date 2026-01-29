c86cfaa54c0ea26da354fd35892bc38ace434202
Natsuka <nnah1v@gmail.com>
2026-01-29 15:53:27 +0900

    2016/1/29 ホテルル

---
+++ moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -0,0 +1,10 @@
+package com.example.moattravel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel.entity.House;
+
+public interface HouseRepository {
+	public interface HouseRepository extends JpaRepository<House, Integer> {
+	}
+}
